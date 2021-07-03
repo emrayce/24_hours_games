1e5bdac890538b7d0a16a800fb10eadc7cef08ea
emrayce <cedric.parpet@orange.fr>
2021-07-03 16:10:13 +0200

    Add: CameraBehaviour: camera now follow the player

---
+++ New Unity Project_hours_games/Assets/Scripts/CameraBehaviour.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraBehaviour : MonoBehaviour
+{
+    public Transform target;
+    public Vector3 offset;
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        Vector3 newPosition = offset + target.position;
+        transform.position = newPosition;
+    }
+}
