c00ba67befa0b9f5a0e4763c13f7bda84b44d5fb
emrayce <cedric.parpet@orange.fr>
2021-07-04 01:52:36 +0200

    Add: MainMenu: create method to navigate through the menu

---
+++ New Unity Project_hours_games/Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+
+    public Image mainMenu;
+    public Image lvlSelection;
+    private string scene = "Lvl_";
+
+    public void Start()
+    {
+        lvlSelection.gameObject.SetActive(false);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public void ToLvlSelection()
+    {
+        mainMenu.gameObject.SetActive(false);
+        lvlSelection.gameObject.SetActive(true);
+    }
+
+    public void ToMainMenu()
+    {
+        lvlSelection.gameObject.SetActive(false);
+        mainMenu.gameObject.SetActive(true);
+    }
+
+    public void SelectLvl(int lvl)
+    {
+        string level = scene + lvl.ToString();
+        SceneManager.LoadScene(level);
+    }
+}
