2dfc2573d36cf3ef36e9972623105571fe66c0d8
emrayce <cedric.parpet@orange.fr>
2021-07-03 17:57:09 +0200

    Add: Floating: script to rotate and float up and down

---
+++ New Unity Project_hours_games/Assets/Scripts/Floating.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Floating : MonoBehaviour
+{
+    public float rotationSpeed;
+    public float FloatingSpeed;
+
+    void Update()
+    {
+        Rotate();
+        Float();
+    }
+
+    void Rotate()
+    {
+        transform.Rotate(0, rotationSpeed * Time.deltaTime, 0, Space.Self);
+    }
+
+    void Float()
+    {
+        transform.Translate(0, FloatingSpeed * Mathf.Cos(Time.time) * Time.deltaTime, 0);
+    }
+}
