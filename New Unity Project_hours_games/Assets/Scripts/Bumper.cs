16d615a8637a6e146c62d1fd4cb505e51c1e9cfe
emrayce <cedric.parpet@orange.fr>
2021-07-04 13:42:49 +0200

    Add: Scripts: bumper behaviour

---
+++ New Unity Project_hours_games/Assets/Scripts/Bumper.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bumper : MonoBehaviour
+{
+    public Vector3 direction;
+    // It needs A LOT of POWER
+    public float power;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("hi");
+            Rigidbody rb = other.gameObject.GetComponent<Rigidbody>();
+            rb.AddForce(direction * power, ForceMode.Impulse);
+        }
+    }
+}
