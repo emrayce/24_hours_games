4bb72f3add431aded53be33fe0ca07e430b99066
emrayce <cedric.parpet@orange.fr>
2021-07-04 06:19:05 +0200

    Add: GateBehaviour: script for gate functionment

---
+++ New Unity Project_hours_games/Assets/Scripts/GateBehaviour.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GateBehaviour : MonoBehaviour
+{
+    public GameManager gameManager;
+    private Animator anim;
+
+    void Awake()
+    {
+        anim = gameObject.GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (gameManager.GotKey())
+        {
+            gameManager.UseKey();
+            anim.SetBool(0, true);
+        }
+    }
+}
