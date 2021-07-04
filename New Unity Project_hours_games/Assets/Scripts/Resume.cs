2250fd6778456571a739428ee945022aef163732
emrayce <cedric.parpet@orange.fr>
2021-07-04 12:38:06 +0200

    Add: UI: create specific files for resume and changing level

---
+++ New Unity Project_hours_games/Assets/Scripts/Resume.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Resume : MonoBehaviour
+{
+    public GameObject pausePanel;
+    public void ResumeGame()
+    {
+        pausePanel.SetActive(false);
+        Time.timeScale = 1f;
+    }
+}
